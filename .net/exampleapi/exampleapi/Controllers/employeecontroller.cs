118732626ada017a7bf7907de3cf930a8528def9
beingrahulramesh <rahulramesh2k1@gmail.com>
2025-05-02 16:24:40 +0530

    changes

---
+++ .net/exampleapi/exampleapi/Controllers/employeecontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace exampleapi.Controllers
+{
+    public class employeecontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
