d8b483a40120fd7e8e7f94ff14ab755e09795831
beingrahulramesh <rahulramesh2k1@gmail.com>
2025-05-03 09:51:59 +0530

    changes

--- .net/exampleapi/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ .net/exampleapi/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2559659c29857b72cd3e5a71470214d12a97f113")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118732626ada017a7bf7907de3cf930a8528def9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
